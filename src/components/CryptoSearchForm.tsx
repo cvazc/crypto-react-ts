9066e755c291140ea7c3e7f1ab335be1b05be12a
Sebastian <contreras.sebastian.dev@gmail.com>
2024-12-26 00:45:36 -0600

    add pair state management to CryptoSearchForm; implement schema for currency pairs

--- src/components/CryptoSearchForm.tsx
+++ src/components/CryptoSearchForm.tsx
@@ -1,14 +1,28 @@
+import { useState, ChangeEvent } from "react"
 import { currencies } from "../data"
 import { useCryptoStore } from "../store"
+import { Pair } from "../types"
 
 export default function CryptoSearchForm() {
     const cryptoCurrencies = useCryptoStore((state) => state.cryptoCurrencies)
+
+    const [pair, setPair] = useState<Pair>({
+        currency: "",
+        cryptoCurency: "",
+    })
+
+    const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        setPair({
+            ...pair,
+            [event.target.name]: event.target.value,
+        })
+    }
 
     return (
         <form className="form">
             <div className="field">
                 <label htmlFor="currency">Moneda:</label>
-                <select name="currency" id="currency">
+                <select name="currency" id="currency" onChange={handleChange}>
                     <option>-- SELECCIONAR --</option>
                     {currencies.map((currency) => (
                         <option key={currency.code} value={currency.code}>
@@ -19,8 +33,12 @@ export default function CryptoSearchForm() {
             </div>
 
             <div className="field">
-                <label htmlFor="cryptocurrency">Criptomoneda:</label>
-                <select name="cryptocurrency" id="cryptocurrency">
+                <label htmlFor="cryptoCurency">Criptomoneda:</label>
+                <select
+                    name="cryptoCurency"
+                    id="cryptoCurency"
+                    onChange={handleChange}
+                >
                     <option>-- SELECCIONAR --</option>
                     {cryptoCurrencies.map((crypto) => (
                         <option
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,9 @@ import { z } from "zod"
 import {
     CurrencySchema,
     CryptoCurrencyResponseSchema,
+    PairSchema,
 } from "../schema/crypto-schema"
 
 export type Currency = z.infer<typeof CurrencySchema>
 export type CryptoCurrency = z.infer<typeof CryptoCurrencyResponseSchema>
+export type Pair = z.infer<typeof PairSchema>
