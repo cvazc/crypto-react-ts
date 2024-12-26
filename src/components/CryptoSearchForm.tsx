191b064ec322eea78255c71fdaad0a2b637f48a9
Sebastian <contreras.sebastian.dev@gmail.com>
2024-12-26 03:14:06 -0600

    add error handling and form submission to CryptoSearchForm; implement fetchCurrentCryptoPrice service

--- src/components/CryptoSearchForm.tsx
+++ src/components/CryptoSearchForm.tsx
@@ -1,15 +1,18 @@
-import { useState, ChangeEvent } from "react"
+import { useState, ChangeEvent, FormEvent } from "react"
 import { currencies } from "../data"
 import { useCryptoStore } from "../store"
 import { Pair } from "../types"
+import ErrorMessage from "./ErrorMessage"
 
 export default function CryptoSearchForm() {
-    const cryptoCurrencies = useCryptoStore((state) => state.cryptoCurrencies)
+    const {cryptoCurrencies, fetchData} = useCryptoStore() 
 
     const [pair, setPair] = useState<Pair>({
         currency: "",
         cryptoCurency: "",
     })
+
+    const [error, setError] = useState("")
 
     const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
         setPair({
@@ -18,12 +21,28 @@ export default function CryptoSearchForm() {
         })
     }
 
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if (Object.values(pair).includes("")) {
+            setError("Todos los campos son obligatorios")
+            return
+        }
+        setError("")
+        fetchData(pair)
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
             <div className="field">
+                {error && <ErrorMessage>{error}</ErrorMessage>}
                 <label htmlFor="currency">Moneda:</label>
-                <select name="currency" id="currency" onChange={handleChange}>
-                    <option>-- SELECCIONAR --</option>
+                <select
+                    name="currency"
+                    id="currency"
+                    onChange={handleChange}
+                    value={pair.currency}
+                >
+                    <option value={""}>-- SELECCIONAR --</option>
                     {currencies.map((currency) => (
                         <option key={currency.code} value={currency.code}>
                             {currency.name}
@@ -38,8 +57,9 @@ export default function CryptoSearchForm() {
                     name="cryptoCurency"
                     id="cryptoCurency"
                     onChange={handleChange}
+                    value={pair.cryptoCurency}
                 >
-                    <option>-- SELECCIONAR --</option>
+                    <option value={""}>-- SELECCIONAR --</option>
                     {cryptoCurrencies.map((crypto) => (
                         <option
                             key={crypto.CoinInfo.FullName}
--- src/services/CryptoService.ts
+++ src/services/CryptoService.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
-import { CryptoCurrenciesResponseSchema } from "../schema/crypto-schema"
+import { CryptoCurrenciesResponseSchema, CryptoPriceSchema } from "../schema/crypto-schema"
+import { Pair } from "../types"
 
 export async function getCryptos() {
     const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD`
@@ -13,3 +14,14 @@ export async function getCryptos() {
         return result.data
     }
 }
+
+export async function fetchCurrentCryptoPrice(pair : Pair) {
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.cryptoCurency}&tsyms=${pair.currency}`
+    const {data : {DISPLAY}} = await axios(url)
+    const result = CryptoPriceSchema.safeParse(DISPLAY[pair.cryptoCurency][pair.currency])
+    
+    if (result.success) {
+        return result.data
+    }
+}
+
